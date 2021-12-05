63182ba3ab6392ccafea3f8e9b18558148528c2e
ZbiFi <zfietkiewicz@gmail.com>
2021-12-05 22:37:52 +0100

    checking what can be done to opt SpouseMM

--- FamilyFunctions.py
+++ FamilyFunctions.py
@@ -1,6 +1,9 @@
 from Enums import MaritalStatus, Sexes
 import Enums
 import Utils
+import time
+from functools import reduce
+
 
 def FindAvailableSpouses (families, person):
 
@@ -26,7 +29,7 @@ def AddToMarriedList (person, spouse):
 
     #if female change lastName => could be modified by culture
     #exception when it is only member of family, don't change
-    if person.familyObjRef.getOriginCulture().getInheritanceBy() == Enums.Sexes.FEMALE:
+    if person.familyObjRef.getOriginCulture().getInheritanceBy() != person.sex:
         spouse.familyObjRef.addMarriedMember(person)
         spouse.familyObjRef.addMarriedMember(spouse)
     else:
@@ -41,7 +44,7 @@ def ChangeFamilyName (person, spouse):
     #if female change lastName => could be modified by culture
     #exception when it is only member of family, don't change
     #CULTURE MIX WHAT IF 2 DIFF CULTURES MEET? => random who is first as person
-    if person.familyObjRef.getOriginCulture().getInheritanceBy() == Enums.Sexes.FEMALE:
+    if person.familyObjRef.getOriginCulture().getInheritanceBy() != person.sex:
         person.lastName = spouseObj.lastName
         person.familyObjRef = spouse.familyObjRef
     else:
@@ -52,6 +55,9 @@ def ChangeFamilyName (person, spouse):
 
 def SpouseMatchmaking (families, people):
 
+    timeTable1 = []
+    timeTable2 = []
+    timeTable3 = []
     for person in people:
         if person.lifeStatus == Enums.LifeStatus.ALIVE and person.age >= 15 and person.spouse is None and (person.maritalStatus == Enums.MaritalStatus.SINGLE or person.maritalStatus == Enums.MaritalStatus.WIDOW or person.maritalStatus == Enums.MaritalStatus.WIDOWER or person.maritalStatus == Enums.MaritalStatus.DIVORCED):
 
@@ -65,10 +71,19 @@ def SpouseMatchmaking (families, people):
                 person.maritalStatus = Enums.MaritalStatus.MARRIED
                 spouseObj.spouse = person
                 spouseObj.maritalStatus = Enums.MaritalStatus.MARRIED
+                start1 = time.time()
                 RemoveFromUnmarriedList(person, spouseObj)
+                end1 = time.time()
+                timeTable1.append(end1 - start1)
                 AddToMarriedList(person, spouseObj)
                 ChangeFamilyName(person, spouseObj)
 
+    if len(timeTable) > 0:
+        print(Average(timeTable)*len(people))
+
+def Average(l):
+    avg = reduce(lambda x, y: x + y, l) / len(l)
+    return avg
 
 
 def FindNextHeir (families, people):
