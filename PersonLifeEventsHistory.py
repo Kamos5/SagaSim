83c54e50e3f29761631754d0d27c90950d9395d0
ZbiFi <zfietkiewicz@gmail.com>
2021-12-07 21:32:34 +0100

    checking what can be done to opt SpouseMM

--- PersonLifeEventsHistory.py
+++ PersonLifeEventsHistory.py
@@ -6,7 +6,17 @@ def adulthoodReached(person):
 
 
 def beenBorn(person, world):
-    person.lifeEvents.append(str(person.firstName) + " had been born into " + str(person.familyName) + " in the year" + str(world.getYear()) + " to " + str(person.father.firstName) + " and " + str(person.mother.firstname))
+
+    if person.father != '':
+        fatherString = str(person.father.firstName)
+    else:
+        fatherString = 'unknown father'
+    if person.mother != '':
+        motherString = str(person.mother.firstName)
+    else:
+        motherString = 'unknown mother'
+
+    person.lifeEvents.append(str(person.firstName) + " had been born into " + str(person.familyName) + " in the year" + str(world.getYear()) + " to " + fatherString + " and " + motherString)
 
 
 def died(person):
